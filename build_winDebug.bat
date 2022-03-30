5aa26992474943aefe729ecd48207fa7e02eac5f
visla-dragon <visla-dragon@visla.us>
2022-03-30 14:07:16 +0800

    update

--- build_winDebug.bat
+++ build_winDebug.bat
@@ -4,16 +4,11 @@ set SRC_DIR=%~dp0
 cd %SRC_DIR%
 rd /s /q %SRC_DIR%\output\win\Debug
 rd /s /q %SRC_DIR%\build
-rem "%1 is used by github action for not copy the vendors"
+
 cmake -B build %SRC_DIR%  -DCMAKE_GENERATOR_TOOLSET=v142,host=x64  ^
                  -G"Visual Studio 16 2019" -A "x64"        ^
                  -DCMAKE_SYSTEM_VERSION=10.0.18362.0       ^
                  -Djinja2cpp_DIR=..\Jinja2Cpp\output\lib\jinja2cpp ^
-				 %1 ^
-				 %2 ^
-				 %3 ^
-				 %4 ^
-				 %5 ^
                  -DCMAKE_BUILD_TYPE=Debug
 cmake --build %SRC_DIR%/build --target clean
 cmake --build %SRC_DIR%/build --config Debug --target install -j 12
